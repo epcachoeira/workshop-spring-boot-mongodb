7271f26f4e42a3fcd84d89c9b7b753d5dcf9c87b
Eduardo Cachoeira <epcachoeira@hotmail.com>
2021-11-29 16:10:58 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/cachoeira/workshopmongo/resources/exception/StandardError.java
@@ -0,0 +1,67 @@
+package com.cachoeira.workshopmongo.resources.exception;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long tiimestamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
+	
+	public StandardError() {
+	}
+
+	public StandardError(Long tiimestamp, Integer status, String error, String message, String path) {
+		super();
+		this.tiimestamp = tiimestamp;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
+
+	public Long getTiimestamp() {
+		return tiimestamp;
+	}
+
+	public void setTiimestamp(Long tiimestamp) {
+		this.tiimestamp = tiimestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+}
