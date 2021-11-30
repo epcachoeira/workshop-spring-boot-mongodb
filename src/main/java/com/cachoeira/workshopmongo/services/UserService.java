26734d9d1a2ffeb2a74c50ed73c02ec1d82dcd42
Eduardo Cachoeira <epcachoeira@hotmail.com>
2021-11-30 10:23:02 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/cachoeira/workshopmongo/services/UserService.java
+++ src/main/java/com/cachoeira/workshopmongo/services/UserService.java
@@ -23,12 +23,17 @@ public class UserService {
 	
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-			
+  		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
 	}
 	
 	public User insert(User obj) {
 		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
 	}
 	
 	public User fromDTO(UserDTO objDto) {
