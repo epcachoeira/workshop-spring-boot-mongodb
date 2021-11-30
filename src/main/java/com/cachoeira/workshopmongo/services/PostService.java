f5c00e131669017903deef15eb35736ea9d0ebe9
Eduardo Cachoeira <epcachoeira@hotmail.com>
2021-11-30 14:20:24 -0300

    Consulta simples com query methods

--- src/main/java/com/cachoeira/workshopmongo/services/PostService.java
+++ src/main/java/com/cachoeira/workshopmongo/services/PostService.java
@@ -1,8 +1,10 @@
 package com.cachoeira.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.cachoeira.workshopmongo.domain.Post;
@@ -18,5 +20,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
   		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+	//	return repo.findByTitleContaining(text);
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
